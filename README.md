52e2e14edf0f00214c54a3641132320aa1d7b391
FedericoMorrillo <federicomorrillo98@gmail.com>
2023-11-07 21:56:03 +0100

    feat: genera biglietto con i calcoli

--- README.md
+++ README.md
@@ -2,8 +2,13 @@
 
 1) creiamo 2 input dove l' utente può inserire all' interno le due informazioni principali (età) (km);
 2) creaiamo un bottone;
-3) salviamo le informazioni inserite negli input all' interno di una variabile;
-4) creiamo le formule per lo sconto in 2 altre variabili;
-5) stampiamo su console il prezzo finale di tutti i casi per verificare che funzioni (formattiamo il valore con massimo 2 decimali);
-6) creiamo il contenitore per i dati stampati;
-7) inseriamo tutti i valori del biglietto all' interno;
+3) aggiungiamo una funzione per attivare un evento al click per prendere i valori da input;
+4) salviamo le informazioni inserite negli input all' interno di una variabile;
+5) creiamo le formule per lo sconto in 2 altre variabili;
+6) stampiamo su console il prezzo finale di tutti i casi per verificare che funzioni (formattiamo il valore con massimo 2 decimali);
+7) creiamo il contenitore per i dati stampati;
+8) inseriamo tutti i contenitori del biglietto all' interno;
+9) importiamo nei contenitori del biglietto i valori salvati nelle variabili js;
+10) diamo al contenitore del biglietto una classe "hidden" in modo da modificare il suo display;
+12) creiamo un secondo bottone;
+13) diamo una funzione che sostituisca la classe "show" con "hidden" al biglietto;
--- js/main.js
+++ js/main.js
@@ -26,16 +26,37 @@ const msgScontoOver = "il prezzo totale è: " + prezzo + " con uno sconto di 40%
 
 //condizioni per gli sconti
 if (età == "minorenne"){
-    prezzo = prezzo - scontoMinori;
+    prezzo = (prezzo - scontoMinori).toFixed(2);
     msg = "il prezzo del biglietto è di: " + prezzo + "€ con uno sconto del 20% sul prezzo totale";
 } else if (età == "over65"){
-    prezzo = prezzo - scontoOver65;
+    prezzo = (prezzo - scontoOver65).toFixed(2);
     msg = "il prezzo del biglietto è di: " + prezzo + "€ con uno scondo del 40% sul prezzo totale";
 }
 
+
+
+//assegnazione valori su biglietto
+document.getElementById("nome").innerHTML = nominativo;
+if(età == "minorenne"){
+    document.getElementById("offerta").innerHTML = "Sconto young"; 
+} else if(età == "over65"){
+    document.getElementById("offerta").innerHTML = "sconto senior";
+} else{
+    document.getElementById("offerta").innerHTML = "nessuno sconto";
+}
+
+document.getElementById("carrozza").innerHTML = Math.floor(Math.random()*10 +1);
+document.getElementById("codice").innerHTML = Math.floor(Math.random()*99999 -10000 +1)+10000;
+document.getElementById("costo").innerHTML = prezzo + "€";
+document.getElementById("biglietto").className = "show";
 //stampa
 console.log( "nome e cognome: "+ nominativo +" km da percorrere: "+ km +" fascia d' età: "+ età);
 console.log(msg);
+
 });
+const btnAnnulla = document.getElementById("annulla") // bottone con evento on click
+btnAnnulla.addEventListener ("click", function(){
+    document.getElementById("biglietto").className = "hidden";
+})
 
 
